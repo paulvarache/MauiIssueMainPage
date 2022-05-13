71302348de4d8eeaea93d3f6d2f7798ca2042432
Paul Varache <varache.paul@gmail.com>
2022-05-13 12:06:52 +0100

    First commit

---
+++ MauiIssueMainPage/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiIssueMainPage;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		Application.Current.MainPage = new NavigationPage(new SecondPage());
+	}
+}
+
---
+++ MauiIssueMainPage/SecondPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiIssueMainPage;
+
+public partial class SecondPage : ContentPage
+{
+	public SecondPage()
+	{
+		InitializeComponent();
+	}
+}
